c962ce10aeecbe3ceea80293e24c4ed712c90a30
eternaluser18 <80708910+eternaluser18@users.noreply.github.com>
2024-02-06 15:11:49 +0100

    updated cmd version

--- cmd_game.py
+++ cmd_game.py
@@ -1,6 +1,7 @@
 import random
 import math
 
+
 def start_game():
     global card_deck
     card_deck = []
@@ -14,25 +15,25 @@ def start_game():
     print("\n")
     name = input("Your name: ")
     global bot
-    bot = Player(name = "botus", n = 0)
+    bot = Player(name="botus", n=0)
     global player
-    player = Player(name = name, n = 0)
+    player = Player(name=name, n=0)
     print(f"{bot.name}: Pleasure to play with you, {player.name}.")
     print(f"{bot.name}: Deck is shuffled already.")
-
 
     draw_cards(card_deck, bot, 4)
     draw_cards(card_deck, player, 4)
     print(f"{bot.name}: Our hands are full of cards. Please start")
     turn_number = 0
-    while(1):
+    while (1):
         if turn_number % 2 == 0:
             player_turn()
         else:
             enemy_turn()
         turn_number += 1
-def end_game():
 
+
+def end_game():
     if bot.chest_number > player.chest_number:
         print("The game has ended")
         print(f"{bot.name} wins. Say your last words.")
@@ -45,16 +46,23 @@ def end_game():
         exit()
     else:
         print("Game isn't finished yet.")
+
+
 def check_chests(player, last_n):
     for card_i in range(last_n):
-        if sum(1 for card in bot.hand if Card.value[card.value] == Card.value[player.hand[-1*(card_i+1)].value]) == 4:
+        if sum(1 for card in bot.hand if
+               Card.value[card.value] == Card.value[player.hand[-1 * (card_i + 1)].value]) == 4:
             bot.chest_number += 1
-            bot.hand = [card for card in player.hand if not Card.value[card.value] == Card.value[player.hand[-1*(card_i+1)]]]
+            bot.hand = [card for card in player.hand if
+                        not Card.value[card.value] == Card.value[player.hand[-1 * (card_i + 1)]]]
             card_i -= 1
+
+
 def player_turn():
-    while(1):
+    while (1):
         line = input(f"{player.name}: ")
-        if line == "hand": player.show_hand()
+        if line == "hand":
+            player.show_hand()
         elif line == "cheat":
             bot.show_hand()
         elif line == "end":
@@ -62,8 +70,11 @@ def player_turn():
         elif line == "guess":
             guess_card(player, bot)
             break
-        elif line == "exit": exit()
-        else: print(f"{bot.name}: I don't understand you. Try again")
+        elif line == "exit":
+            exit()
+        else:
+            print(f"{bot.name}: I don't understand you. Try again")
+
 
 def enemy_turn():
     random.shuffle(bot.hand)
@@ -73,17 +84,17 @@ def enemy_turn():
     player_suit_list = [suit for suit in Card.suit if suit not in temp_suit_of_v]
 
     a = len(temp)
-    ar = random.randrange(1, 5-a)
+    ar = random.randrange(1, 5 - a)
     print(f"{bot.name}: Do you have {v}'s?")
     correct_answer = any(Card.value[card.value] == v for card in player.hand)
-    while(1):
+    while (1):
         answer = input(f"{player.name}: ")
         if answer == "yes" and correct_answer == 1:
             print(f"{bot.name}: Good.")
             break
         elif answer == "no" and correct_answer == 1:
             print(f"{bot.name}: LIAR!")
-            for i in range (len((player.hand)/2)):
+            for i in range(len((player.hand) / 2)):
                 steal_cards(bot, player, Card.value[player.hand[0]])
             print(f"Half of your cards goes to {bot.name}")
             return
@@ -96,13 +107,14 @@ def enemy_turn():
             print(f"{bot.name}: Okay. Your turn.")
             draw_cards(card_deck, bot, 1)
             return
-        else: print(f"{bot.name}: I didn't understand ypu. Try again.")
+        else:
+            print(f"{bot.name}: I didn't understand ypu. Try again.")
 
     if a == 3:
         print(f"{bot.name}: I know your card. It's {v} of {player_suit_list.pop()}")
     else:
-        for x in range(4-a):
-            if x == 4-a:
+        for x in range(4 - a):
+            if x == 4 - a:
                 print(f"{bot.name}: And last card. {Card.value[v.value]} of {temp_suit_of_v.pop()}")
             else:
                 temp1 = temp_suit_of_v.pop()
@@ -117,7 +129,6 @@ def enemy_turn():
                         print(f"{bot.name}: LIAR!")
                         temp2 = math.ceil(len(player.hand) / 2)
                         for i in range(temp2):
-
                             steal_cards(bot, player, Card.value[player.hand[0]])
 
                         print(f"More then half of your cards goes to {bot.name}")
@@ -131,48 +142,52 @@ def enemy_turn():
                         print(f"One of your cards goes to {bot.name}")
                         if sum(1 for card in bot.hand if Card.value[card.value] == Card.value[bot.hand[-1].value]) == 4:
                             bot.chest_number += 1
-                            bot.hand = [card for card in bot.hand if not Card.value[card.value] == Card.value[bot.hand[-1]]]
+                            bot.hand = [card for card in bot.hand if
+                                        not Card.value[card.value] == Card.value[bot.hand[-1]]]
                             if player.chest_number + bot.chest_number == 13:
                                 end_game()
                         return
                     elif answer == "no" and correct_answer == 0:
                         print(f"{bot.name}: Okay. Your turn.")
                         draw_cards(card_deck, bot, 1)
-                        if sum(1 for card in player.hand if Card.value[card.value] == Card.value[player.hand[-1].value]) == 4:
+                        if sum(1 for card in player.hand if
+                               Card.value[card.value] == Card.value[player.hand[-1].value]) == 4:
                             player.chest_number += 1
-                            player.hand = [card for card in player.hand if not Card.value[card.value] == Card.value[player.hand[-1]]]
+                            player.hand = [card for card in player.hand if
+                                           not Card.value[card.value] == Card.value[player.hand[-1]]]
                             if player.chest_number + bot.chest_number == 13:
                                 end_game()
                         return
                     else:
                         print(f"{bot.name}: I didn't understand ypu. Try again.")
 
+
 class Card:
     suit = ["spades",
-                "hearts",
-                "diamonds",
-                "clubs"]
+            "hearts",
+            "diamonds",
+            "clubs"]
 
     value = [None, None, "2", "3",
-                 "4", "5", "6", "7",
-                 "8", "9", "10",
-                 "Jack", "Queen",
-                 "King", "Ace"]
+             "4", "5", "6", "7",
+             "8", "9", "10",
+             "Jack", "Queen",
+             "King", "Ace"]
 
     def __init__(self, v, s):
         self.value = v
         self.suit = s
 
 
-
-
 def draw_cards(deck, player, n):
     for x in range(n):
         if deck:
-                card_from_deck = deck.pop()
-                player.add_card_to_hand(card_from_deck)
+            card_from_deck = deck.pop()
+            player.add_card_to_hand(card_from_deck)
         else:
             print("empty")
+
+
 class Player:
     def __init__(self, name, n):
         self.name = name
@@ -189,18 +204,18 @@ class Player:
         for card in self.hand:
             print(f"{Card.value[card.value]} {Card.suit[card.suit]}")
 
-def guess_card(guesser, guessed):
 
+def guess_card(guesser, guessed):
     g_value = input("(value)Do you have ")
 
     if not any(Card.value[card.value] == g_value for card in guesser.hand):
         print(f"You break the rule, so now is my turn.")
         return
     if not any(Card.value[card.value] == g_value for card in guessed.hand):
-       print(f"{bot.name}: No. My turn.")
-       draw_cards(card_deck, player, 1)
-       print(f"You took a {Card.value[player.hand[-1].value]} of {Card.suit[player.hand[-1].suit]}")
-       return
+        print(f"{bot.name}: No. My turn.")
+        draw_cards(card_deck, player, 1)
+        print(f"You took a {Card.value[player.hand[-1].value]} of {Card.suit[player.hand[-1].suit]}")
+        return
     print("yes")
     g_amount = int(input("in amount of "))
 
@@ -231,13 +246,14 @@ def guess_card(guesser, guessed):
             print(f"You took a {Card.value[player.hand[-1].value]} of {Card.suit[player.hand[-1].suit]}")
             if sum(1 for card in player.hand if Card.value[card.value] == Card.value[player.hand[-1].value]) == 4:
                 player.chest_number += 1
-                player.hand = [card for card in player.hand if not Card.value[card.value] == Card.value[player.hand[-1]]]
+                player.hand = [card for card in player.hand if
+                               not Card.value[card.value] == Card.value[player.hand[-1]]]
                 if player.chest_number + bot.chest_number == 13:
                     end_game()
             return
         if temp == len(temp_card_list):
             print("okay, take it")
-            steal_cards(stealer = guesser, stealed = guessed, value = g_value)
+            steal_cards(stealer=guesser, stealed=guessed, value=g_value)
             if sum(1 for card in player.hand if Card.value[card.value] == g_value) == 4:
                 player.chest_number += 1
                 player.hand = [card for card in player.hand if not Card.value[card.value] == g_value]
@@ -246,10 +262,13 @@ def guess_card(guesser, guessed):
         else:
             print("okay, next")
 
+
 def steal_cards(stealer, stealed, value):
     cards = [card for card in stealed.hand if Card.value[card.value] == value]
     stealer.hand += cards
     stealed.hand = [card for card in stealed.hand if card not in cards]
+
+
 def show_rules():
     print("Hello, Player.")
     print("This game is simple. There is regular deck of cards.")
@@ -267,14 +286,11 @@ print("Welcome.")
 while (1):
     print("[rules], [start], [exit].")
     line = input("me: ")
-    if line == "rules": show_rules()
-    elif line == "start": start_game()
-    elif line == "exit": exit()
-    else: print("I don't understand you, try again.")
-
-
-
-
-
-
-
+    if line == "rules":
+        show_rules()
+    elif line == "start":
+        start_game()
+    elif line == "exit":
+        exit()
+    else:
+        print("I don't understand you, try again.")
